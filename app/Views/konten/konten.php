4f4d1db6690836669f1a94962644ca8afd7e82d4
unknown <asrulpuadi@gmail.com>
2022-02-04 20:37:14 +0700

    First Push

---
+++ app/Views/konten/konten.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layout')?>
+<?= $this->section('content') ?>
+
+	<div class="starter-template">
+		<h1>Bootstrap starter template</h1>
+        <p class="lead">My name is <?= $nama?></p>
+	</div>
+	
+<?= $this->endSection()?>
